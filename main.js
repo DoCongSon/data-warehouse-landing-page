a020a8ed9e290c388450c38f2826e75a4ab8ebe0
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-10-22 22:23:38 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-
+function app() {
+  var arrItem = document.querySelectorAll(".slides-item");
+  var arrDot = document.querySelectorAll(".dot");
+  var items = [...arrItem];
+  var dots = [...arrDot];
+  function scrollTo(item) {
+    item.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "start",
+    });
+  }
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", function (e) {
+      const dotIndex = Number(e.target.dataset.index);
+      scrollTo(items[dotIndex]);
+      dots.forEach((e) => {
+        e.classList.remove("dot--active");
+      });
+      dot.classList.add("dot--active");
+    });
+  });
+}
+app();
