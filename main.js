a9dc934040fc0a3b6dca01731c984caa393a940c
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-10-29 21:28:26 +0700

    add slider

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
-function app() {
-  var arrItem = document.querySelectorAll(".slides-item");
-  var arrDot = document.querySelectorAll(".dot");
-  var items = [...arrItem];
-  var dots = [...arrDot];
-  function scrollTo(item) {
-    item.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "start",
-    });
+function slide() {
+  const slide = document.querySelector(".slides");
+  const nextArrow = document.querySelector(".slides-arrow--next");
+  const prevArrow = document.querySelector(".slides-arrow--prev");
+  const arrItem = document.querySelectorAll(".slides-item");
+  const arrDot = document.querySelectorAll(".dot");
+  const item = [...arrItem];
+  const dots = [...arrDot];
+  const gapLength = 40;
+  const itemLength = item[0].offsetWidth + gapLength;
+  const slideLength = itemLength * item.length;
+  var index = 0;
+  var translateX = 0;
+  function activeDot(index) {
+    dots.forEach(function(dot) {
+      dot.classList.remove('dot--active');
+    })
+    dots[index].classList.add('dot--active');
   }
-  dots.forEach((dot, index) => {
+  dots.forEach(function (dot) {
     dot.addEventListener("click", function (e) {
-      const dotIndex = Number(e.target.dataset.index);
-      scrollTo(items[dotIndex]);
-      dots.forEach((e) => {
-        e.classList.remove("dot--active");
-      });
-      dot.classList.add("dot--active");
-    });
+      index = e.target.dataset.index;
+      translateX = -index*itemLength;
+      slide.style.transform = `translateX(${translateX}px)`;
+      activeDot(index);
+    })
+  })
+  prevArrow.addEventListener("click", function (e) {
+    handlePrevSlide();
   });
+  nextArrow.addEventListener("click", function (e) {
+    handleNextSlide();
+  });
+  function handleNextSlide() {
+    if (index >= item.length - 1) {
+      index = 0;
+      translateX = 0;
+      slide.style.transform = `translateX(${translateX}px)`;
+      console.log(index);
+      activeDot(index);
+      return;
+    }
+    index++;
+    translateX -= itemLength;
+    slide.style.transform = `translateX(${translateX}px)`;
+    console.log(index);
+    activeDot(index);
+  }
+  function handlePrevSlide() {
+    if (index <= 0) {
+      index = item.length - 1;
+      translateX = -slideLength + itemLength;
+      slide.style.transform = `translateX(${translateX}px)`;
+      console.log(index);
+      activeDot(index);
+      return;
+    }
+    index--;
+    translateX += itemLength;
+    slide.style.transform = `translateX(${translateX}px)`;
+    console.log(index);
+    activeDot(index);
+  }
 }
-app();
+slide();
